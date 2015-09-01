2f736b849041a5036af7ba8f06260d24d451c9a1
samurai20000 <samurai20000@gmail.com>
2015-09-01 23:06:41 +0200

    Move index.html to src/ and add task 'compile-html'

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var watch = require('gulp-watch');
 
 gulp.task('compile', 
     [
-        'compile-es6'
+        'compile-es6',
+        'compile-html'
     ]
 );
 
@@ -13,6 +14,11 @@ gulp.task('compile-es6', function() {
     return gulp.src('src/**/*.js')
         .pipe(plumber())
         .pipe(babel())
+        .pipe(gulp.dest('./app'));
+});
+
+gulp.task('compile-html', function() {
+    return gulp.src('src/**/*.html')
         .pipe(gulp.dest('./app'));
 });
 
